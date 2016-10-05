e6ce76164402f1cfb754758c952e84ac4eb5e117
楼教主 <cik520@qq.com>
2016-10-06 01:38:36 +0800

    从写了抓取功能

--- index.js
+++ index.js
@@ -1,91 +1,69 @@
 'use strict'
 
-const urlResolve = require('url').resolve
-const request = require('superagent')
-const cheerio = require('cheerio')
-const parallel = require("async/parallel")
 const tinyDB = require('tinydb')
+const Crawler = require('crawler')
 
-const url = 'http://www.shumilou.co/xiaohuadetieshengaoshou/'
+const uri = 'http://m.69shu.com/txt/1464.htm'
 
 const db = new tinyDB()
 const db_key = 'xiaohuadetieshengaoshou'
 
+let list = [] // 章节列表
+let onDrain_cb // 结束回掉
+
+const c = new Crawler({
+  forceUTF8: true,
+  onDrain: function () {
+    c.pool.destroyAllNow()
+    onDrain_cb && onDrain_cb(list)
+  }
+})
 
 /**
  * 抓取列表
  *
  * @param  {Function} cb    回调函数
- * @param  {[type]}   limit 抓取条，默认5条
- *
+ * @param  {Numer}   limit 抓取条，默认5条
  */
 function fatchList(cb, limit) {
-  limit = limit || 5;
+  onDrain_cb = cb
+  limit = limit || 5
 
-  fatch(url, str => {
-    let $ = cheerio.load(str) // 解析html
-    let $list = $('.zl>a').slice(-limit) // 截取末尾几条数据
-    let tasks = [] // 保存并发任务
+  c.queue([{
+    uri,
+    callback: (error, result, $) => {
+      let $list = $('.chapter_list01>ul>li>a')
+      let last = db.get(db_key) // 最新章节
 
-    let last = db.get(db_key) // 最新章节
+      $list.each((idx, el) => {
+        let title = $(el).text()
+        let href = $(el).attr('href')
 
-    // 倒序
-    Array.prototype.reverse.call($list).each((idx, el) => { // 处理小说任务
-      let title = $(el).text()
-      let href = $(el).prop('href')
+        if (last === href) { // 处理老章节
+          return false
+        }
 
-      if (last === href) { // 处理老章节
-        return false
-      }
-
-      tasks.push(cb => { // 添加并发任务
-        fatchContent(title, urlResolve(url, href), str => {
-          cb(null, str)
-        })
+        fatchContent(title, href, cb)
       })
-    })
 
-    db.set(db_key, $list.eq(0).prop('href')) // 保存最新章节
-
-    parallel(tasks, (err, results) => { // 并发抓取内容
-      // console.log(results)
-      cb(results)
-    })
-  })
+      db.set(db_key, $list.eq(0).attr('href')) // 保存最新章节
+    }
+  }])
 }
 
 /**
  * 抓取小说内容
  *
- * @param  {String}   url 小说章节地址
- * @param  {Function} cb  回调函数
+ * @param  {String}   title 小说章节标题
+ * @param  {String}   uri   小说章节地址
+ * @param  {Function} cb    回调函数
  */
-function fatchContent(title, url, cb) {
-  fatch(url, str => {
-    let $ = cheerio.load(str) // 解析html
-    let text = $('#content>p').slice(0, -1).text()
-
-    // console.log(text)
-    cb({title, text})
-  })
-}
-
-
-/**
- * 抓取 url 内容
- *
- * @param  {String}   url 地址
- * @param  {Function} cb  回调函数
- */
-function fatch(url, cb) {
-  request.get(url).end((err, res) => {
-    if (err) {
-      throw err
+function fatchContent(title, uri) {
+  c.queue([{
+    uri,
+    callback: (error, result, $) => {
+      let text = '<p>' + $('#chapcont').text().replace(/[\xa0]+/g, '</p><p>') + '</p>'
+      list.push({title, text})
     }
-
-    cb(res.text)
-  })
+  }])
 }
-
-// 导出接口
-module.exports = fatchList
